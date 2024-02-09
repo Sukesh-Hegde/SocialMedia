c25027c4de619e368929c5ce5ef6c1ab2a88f836
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-02-09 23:34:23 +0530

    controller

--- src/features/posts/posts.Repository.js
+++ src/features/posts/posts.Repository.js
@@ -1,18 +1,22 @@
 import { postSchema } from "./posts.schema.js";
 import mongoose from "mongoose";
 
-
-
 // creating model from schema.
-const PostModel = mongoose.model('Post', postSchema);
+const PostModel = mongoose.model("Post", postSchema);
 
 // const LikeModel = mongoose.model('Like',likeSchema);
 
 // const CommentModel = mongoose.model('Comment',commentSchema)
 
-export default class PostRepository{
-
-    async add(post){
+export default class PostRepository {
+  async add(post) {
+    try {
+      const newProduct = new PostModel(post);
+      return await newProduct.save();
         
+    } catch (err) {
+      console.log(err);
+      throw new ApplicationError("Something went wrong with database", 500);
     }
+  }
 }
--- src/features/posts/posts.controller.js
+++ src/features/posts/posts.controller.js
@@ -1,28 +1,24 @@
 import PostRepository from "./posts.Repository.js";
 
-
 export default class PostController {
-    constructor() {
-        this.postRepository = new PostRepository();
-      }
+  constructor() {
+    this.postRepository = new PostRepository();
+  }
   async createPost(req, res) {
     const { caption } = req.body;
     const userID = req.userID; //requesting directly from token
 
     const newPost = {
-      userID,
+      userId: userID,
       caption,
-      filename: req.file.filename,
+      imageUrl: req.file.filename,
     };
     try {
-        const post = await this.postRepository.add(newPost);
-        res.status(200).send(post);
-
-
+      const post = await this.postRepository.add(newPost);
+      res.status(200).send(post);
     } catch (err) {
-        console.log(err);
-        return res.status(200).send("Something went wrong while creating post");
-      }
+      console.log(err);
+      return res.status(200).send("Something went wrong while creating post");
     }
-  
+  }
 }
